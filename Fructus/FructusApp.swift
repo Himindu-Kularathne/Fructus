88d3f614964ab1bd20fccae5784b52379cb0a42e
Himindu <himindukularathne@gmail.com>
2023-12-20 13:01:35 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Himindu Kularathne on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
